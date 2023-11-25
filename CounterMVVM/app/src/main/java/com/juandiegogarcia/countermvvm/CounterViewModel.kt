661aff23357f72a1802d185cb941c325e504c829
juandiego11116@gmail.com <71513912+juandiego11116@users.noreply.github.com>
2023-11-25 10:17:53 -0500

    Feat: Implement MVVM

--- CounterMVVM/app/src/main/java/com/juandiegogarcia/countermvvm/CounterViewModel.kt
+++ CounterMVVM/app/src/main/java/com/juandiegogarcia/countermvvm/CounterViewModel.kt
@@ -4,15 +4,18 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class CounterViewModel : ViewModel(){
-    private val _count = mutableStateOf(0)
+class CounterViewModel() : ViewModel(){
+    private val _repository: CounterRepository = CounterRepository()
+    private val _count = mutableStateOf(_repository.getCounter().count)
 
     val count: MutableState<Int> = _count
 
     fun increment(){
-        count.value++
+        _repository.incrementCounter()
+        _count.value = _repository.getCounter().count
     }
     fun decrement(){
-        count.value--
+        _repository.decrementCounter()
+        _count.value = _repository.getCounter().count
     }
 }
